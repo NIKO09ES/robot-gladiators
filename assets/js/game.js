0a88eb621dc37819d204812cefaddc87d108d342
NIKO09ES <nicolasesquivelruiz@gmail.com>
2020-06-03 22:01:42 -0500

    tandom health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,7 +11,14 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 var confirmSkip = true;
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
 
+  return value;
+};
+
+randomNumber(40, 60);
 
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
@@ -27,14 +34,16 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -53,7 +62,9 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -81,6 +92,8 @@ for(var i = 0; i < enemyNames.length; i++) {
         break;
     }
 };
+
+
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
@@ -127,7 +140,7 @@ var shop = function() {
 // function to start a new game
 var startGame = function() {
   // reset player stats
-  playerHealth = 100;
+  enemyHealth = randomNumber(40,60);
   playerAttack = 10;
   playerMoney = 10;
 
@@ -139,7 +152,7 @@ var startGame = function() {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
+      enemyHealth = Math.floor(Math.random() * 21) + 40;
 
       fight(pickedEnemyName);
       // if we're not at the last enemy in the array
@@ -186,3 +199,4 @@ endGame();
 
 //play again
 startGame();
+
